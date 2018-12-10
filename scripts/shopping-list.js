bad56466868128601c04598d25719fd0730be64d
John Young <john.young@spreadstreet.io>
2018-12-10 11:21:53 -0500

    Added store.js and moved global variable

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,4 +1,5 @@
-/* global store, cuid */
+/* global $, store, cuid */
+'use strict';
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
---
+++ scripts/store.js
@@ -0,0 +1,21 @@
+/* global cuid */
+'use strict';
+
+const store = (function () {
+
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false;
+  const searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+
+}() );
